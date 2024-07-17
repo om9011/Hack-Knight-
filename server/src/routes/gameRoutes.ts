9cc11078950a2a3f118132e48f20010a3cb0b803
Om-Gujarathi <veryhappyom@gmail.com>
2024-07-17 19:26:41 +0530

    added start route

--- server/src/routes/gameRoutes.ts
+++ server/src/routes/gameRoutes.ts
@@ -3,11 +3,13 @@ import {
   getGameStatus,
   invest,
   payOffLiability,
+  startGame,
 } from "../controllers/gameController";
 
 const router = express.Router();
 
 router.get("/status", getGameStatus);
+router.get("/start", startGame);
 router.post("/invest", invest);
 router.post("/pay-off-liability", payOffLiability);
 
