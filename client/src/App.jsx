299590df2ab8860bdb5fe00a1966dcd0c69e52fb
Om Gore <omgore4307@gmail.com>
2024-07-17 17:51:10 +0530

    Project Init

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import Game from './Components/Game'
+
+function App() {
+
+
+  return (
+    <>
+     <Game />
+    </>
+  )
+}
+
+export default App
