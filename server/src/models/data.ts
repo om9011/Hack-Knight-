4e0b778a1503e2f5e6ff6f7dd13885f32e9b29c0
Om-Gujarathi <veryhappyom@gmail.com>
2024-07-18 13:35:18 +0530

    fix

--- server/src/models/data.ts
+++ server/src/models/data.ts
@@ -30,7 +30,7 @@ export const hintsData = [
 //  gold, stocks , bitcoin, real estate, bank deposit
 
 export const multiplier = {
-  1: [1.01, 1.01, 1.03, 1.07, 1.02],
+  1: [1.01, -1.01, -0.90, 1.07, 1.02],
   2: [1.07, 0.97, 0.99, 0.98, 1],
   3: [1.0, 1.03, 1.05, 1.1, 1.01],
 } as { [key: number]: number[] };
